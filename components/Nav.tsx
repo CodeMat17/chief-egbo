68a067761a64b0fbf012d6aab1f40b163b1a5769
CodeMat17 <codemat.biz@gmail.com>
2025-02-18 19:57:43 +0100

    updated

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,9 +1,9 @@
-'use client'
+"use client";
 
-import { useEffect, useState } from "react";
 import ThemeToggle from "@/components/theme/theme-toggle";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import NavSheet from "./NavSheet";
 import { Button } from "./ui/button";
 
@@ -18,6 +18,11 @@ const links = [
 ];
 
 const Nav = () => {
+  // Since usePathname() doesn't include the hash, we use state to track the current hash.
+  const [currentHash, setCurrentHash] = useState<string>(
+    typeof window !== "undefined" ? window.location.hash : ""
+  );
+
   const [isSheetOpen, setIsSheetOpen] = useState(false);
 
   const handleScroll = (
@@ -42,42 +47,31 @@ const Nav = () => {
 
       window.history.pushState({}, "", hash);
       setIsSheetOpen(false); // Close mobile menu after navigation
+      setCurrentHash(hash);
     }
   };
 
   useEffect(() => {
-    const handleHash = () => {
-      const hash = window.location.hash;
-      if (hash) {
-        const targetElement = document.getElementById(hash.replace("#", ""));
-        if (targetElement) {
-          const nav = document.querySelector("nav");
-          const offset = nav?.clientHeight || 80;
-          const elementPosition =
-            targetElement.getBoundingClientRect().top + window.scrollY;
-          const offsetPosition = elementPosition - offset;
-          window.scrollTo({ top: offsetPosition });
-        }
-      }
+    const handleHashChange = () => {
+      setCurrentHash(window.location.hash);
     };
 
-    handleHash();
-    window.addEventListener("hashchange", handleHash);
-    return () => window.removeEventListener("hashchange", handleHash);
+    handleHashChange();
+    window.addEventListener("hashchange", handleHashChange);
+    return () => window.removeEventListener("hashchange", handleHashChange);
   }, []);
 
   return (
     <nav className='fixed top-0 z-50 w-full p-4 bg-gradient-to-b from-black/90 backdrop-blur-[0.5px]'>
       <div className='w-full max-w-7xl mx-auto flex items-center justify-between'>
-          <Image
-            alt='logo'
-            priority
-            src='/logo.webp'
-            width={70}
-            height={70}
-            className='aspect-square object-cover rounded-full border-4 border-amber-400'
-          />
-   
+        <Image
+          alt='logo'
+          priority
+          src='/logo.webp'
+          width={70}
+          height={70}
+          className='aspect-square object-cover rounded-full border-4 border-amber-400'
+        />
         <div className='flex items-center gap-6'>
           <ThemeToggle />
           <NavSheet
@@ -87,20 +81,25 @@ const Nav = () => {
             onLinkClick={handleScroll}
           />
           <div className='hidden lg:flex items-center gap-2'>
-            {links.map((link, i) => (
-              <Button
-                key={i}
-                asChild
-                variant='ghost'
-                className='text-white hover:text-amber-400 transition-colors'>
-                <Link
-                  href={link.href}
-                  onClick={(e) => handleScroll(e, link.href)}
-                  scroll={false}>
-                  {link.tag}
-                </Link>
-              </Button>
-            ))}
+            {links.map((link, i) => {
+              const isActive = link.href === currentHash;
+              return (
+                <Button
+                  key={i}
+                  asChild
+                  variant='ghost'
+                  className={`transition-colors transform duration-500 rounded-xl text-white hover:text-amber-400  ${
+                    isActive ? " bg-amber-700/30" : "text-white "
+                  }`}>
+                  <Link
+                    href={link.href}
+                    onClick={(e) => handleScroll(e, link.href)}
+                    scroll={false}>
+                    {link.tag}
+                  </Link>
+                </Button>
+              );
+            })}
           </div>
         </div>
       </div>
--- components/NavSheet.tsx
+++ components/NavSheet.tsx
@@ -1,14 +1,10 @@
-import {
-  Sheet,
-  SheetContent,
-  // SheetDescription,
-  // SheetHeader,
-  // SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
+"use client";
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { AlignLeftIcon } from "lucide-react";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { useEffect, useState } from "react";
 
 interface NavSheetProps {
   links: { tag: string; href: string }[];
@@ -23,6 +19,21 @@ const NavSheet = ({
   onOpenChange,
   onLinkClick,
 }: NavSheetProps) => {
+  const [currentHash, setCurrentHash] = useState<string>(
+    typeof window !== "undefined" ? window.location.hash : ""
+  );
+
+  useEffect(() => {
+    const handleHashChange = () => {
+      setCurrentHash(window.location.hash);
+    };
+
+    // Initial update.
+    handleHashChange();
+    window.addEventListener("hashchange", handleHashChange);
+    return () => window.removeEventListener("hashchange", handleHashChange);
+  }, []);
+
   return (
     <section className='lg:hidden'>
       <Sheet open={isOpen} onOpenChange={onOpenChange}>
@@ -33,19 +44,27 @@ const NavSheet = ({
         </SheetTrigger>
         <SheetContent>
           <div className='flex flex-col gap-4 mt-8'>
-            {links.map((link, i) => (
-              <Link
-                key={i}
-                href={link.href}
-                onClick={(e) => {
-                  onLinkClick(e, link.href);
-                  onOpenChange(false); // Close sheet after click
-                }}
-                scroll={false}
-                className='hover:text-amber-400 transition-colors px-4 py-2'>
-                {link.tag}
-              </Link>
-            ))}
+            {links.map((link, i) => {
+              const isActive = link.href === currentHash;
+              return (
+                <Link
+                  key={i}
+                  href={link.href}
+                  onClick={(e) => {
+                    onLinkClick(e, link.href);
+                    setCurrentHash(link.href); // Update active hash immediately.
+                    onOpenChange(false); // Close sheet after click
+                  }}
+                  scroll={false}
+                  className={`px-4 py-2 transition-colors rounded-xl ${
+                    isActive
+                      ? "text-amber-600 bg-amber-700/10 dark:bg-amber-700/20"
+                      : " hover:text-amber-400"
+                  }`}>
+                  {link.tag}
+                </Link>
+              );
+            })}
           </div>
         </SheetContent>
       </Sheet>
